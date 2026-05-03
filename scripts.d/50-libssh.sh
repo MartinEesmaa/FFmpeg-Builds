b7d2d6599fa4a4016aec86dc64f67b98ddd65e9c
MartinEesmaa <martin.eesmaa@protonmail.com>
2026-05-03 06:05:12 +0300

    Let's try revert libssh back to fix Windows issues

--- scripts.d/50-libssh.sh
+++ scripts.d/50-libssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://gitlab.com/libssh/libssh-mirror.git"
-SCRIPT_COMMIT="ad84df197c27e2c6ade847192003111798168515"
+SCRIPT_COMMIT="3154a4ab8d3277d1cabe028cd3c0841e945f6863"
 
 ffbuild_depends() {
     echo base
@@ -19,13 +19,8 @@ ffbuild_dockerbuild() {
     export CFLAGS="$CFLAGS -Dmd5=libssh_md5"
 
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DHAVE_STRNDUP=YES -DBUILD_SHARED_LIBS=OFF -DWITH_EXAMPLES=OFF -DWITH_SERVER=OFF -DWITH_SFTP=ON -DWITH_ZLIB=ON \
+        -DBUILD_SHARED_LIBS=OFF -DWITH_EXAMPLES=OFF -DWITH_SERVER=OFF -DWITH_SFTP=ON -DWITH_ZLIB=ON \
         ..
-
-    # Fix compilation on windows, mingw exports the symbol, but the header only shows it for c23.
-    # Since the cmake script only checks for the symbol, it succeeds. But then fails to build.
-    echo '#include <stddef.h>' >> config.h
-    echo 'char * strndup(const char *s, size_t c);' >> config.h
 
     ninja -j$(nproc)
     DESTDIR="$FFBUILD_DESTDIR" ninja install
